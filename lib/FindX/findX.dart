5e23b2ad43b595aac2ccfdfa18ff10f51fddaf74
Nakul Dev <nakuldev1561@gmail.com>
2024-11-06 23:41:48 +0530

    First Commit

---
+++ lib/FindX/findX.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class findX extends StatelessWidget {
+  const findX({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
